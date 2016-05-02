9d89af5ea3f57a9c1b7e6e2614a203d99969675a
geofflightbourn <geoffrey.lightbourn@loop.colum.edu>
2016-05-01 23:59:50 -0500

    adding custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,10 +1,17 @@
-
 module.exports = function(robot) {
 
 	// robot.hear() will have the robot listen in on any channel it's in
-	robot.hear(/badger/i, function(response) {
-		response.send("Badgers? BADGERS? WE DON'T NEED NO STINKIN BADGERS")
+	robot.hear(/hello/i, function(response) {
+		response.send("Hi, how are you? 1 - good or 2 - bad?")
 	})
+
+	if (robot.hear(/1/i) {
+		response()
+		response.send("Great!")
+	}	else if (robot.hear(/2/i) {
+		response()
+		respond.send("Sorry to hear that...")
+	}
 
 	robot.hear(/i like (.+)/i, function(response) {
 		var like = response.match[1]
@@ -13,9 +20,11 @@ module.exports = function(robot) {
 	})
 
 	// robot.respond() means the robot will only reply when at-mentioned or DMed
-	robot.respond(/who should i call on today\?/i, function(response) {
-		var user = robot.brain.userForName('gordo')
+	robot.respond(/call on geoff/i, function(response) {
+		var user = robot.brain.userForName('geoff')
 		response.reply('Call on ' + user.real_name + '!')
 	})
+
+
 
 }
