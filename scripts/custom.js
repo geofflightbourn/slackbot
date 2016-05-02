632b1d11aab2b6401a5a31d0e58b4911aa6af0d5
geofflightbourn <geoffrey.lightbourn@loop.colum.edu>
2016-05-02 17:51:35 -0500

    fixed picture send 2

--- scripts/custom.js
+++ scripts/custom.js
@@ -9,11 +9,11 @@ module.exports = function(robot) {
     if (response.match[1] === '1') {
        response.send("Great!")
    } else if (response.match[1] === '2') {
-     robot.http("http://www.edgewoodyachtclub.com/wp-content/uploads/2016/01/beer.gif").get()(function(err, res, body){
+     //robot.http("http://www.edgewoodyachtclub.com/wp-content/uploads/2016/01/beer.gif").get()(function(err, res, body){
       response.send("Sorry to hear that... have a beer.")
+      response.send("http://www.edgewoodyachtclub.com/wp-content/uploads/2016/01/beer.gif")
      })
-
-   }
+    }
   })
 
 	robot.hear(/i like (.+)/i, function(response) {
@@ -24,7 +24,7 @@ module.exports = function(robot) {
 	robot.respond(/greet random/i, function(response) { //greets a random classmate
     var userIds = Object.keys(robot.brain.data.users);
     var randomUsername = robot.brain.data.users[userIds[Math.floor(Math.random() * userIds.length)]].name
-		response.reply('hello ' + user.userIds + '!')
+		response.reply('hello @' + randomUsername + '!')
 	})
 
   robot.hear(/give me people/i, function(response) { //gives list of classmate usernames
