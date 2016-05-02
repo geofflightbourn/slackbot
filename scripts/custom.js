f9938bb84d4538d2dea4682181a54ed1ca181459
geofflightbourn <geoffrey.lightbourn@loop.colum.edu>
2016-05-02 16:17:22 -0500

    fixed if statement

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,26 +1,26 @@
 module.exports = function(robot) {
 
-	// robot.hear() will have the robot listen in on any channel it's in
+
 	robot.hear(/hello/i, function(response) {
-		response.send("Hi, how are you? 1 - good or 2 - bad?")
+		response.send("Hi, how are you? 1 - Happy or 2 - Sad?")
 	})
 
   robot.hear(/(1|2)/i, function(response) {
     if (response.match[1] === '1') {
        response.send("Great!")
-   } else if (response.match[1] === '2') {
-       respond.send("Sorry to hear that...")
+   } else if (response.match[2] === '2') {
+     robot.http("http://www.edgewoodyachtclub.com/wp-content/uploads/2016/01/beer.gif")
+     .get() (err, res, body)
+      respond.send("Sorry to hear that...")
    }
   })
 
 	robot.hear(/i like (.+)/i, function(response) {
 		var like = response.match[1]
-		// response.reply() will have the robot at-mention you when it responds
 		response.reply('I also like ' + like)
 	})
 
-	// robot.respond() means the robot will only reply when at-mentioned or DMed
-	robot.respond(/call on geoff/i, function(response) {
+	robot.respond(/greet geoff/i, function(response) {
 		var user = robot.brain.userForName('geoff')
 		response.reply('Call on ' + user.real_name + '!')
 	})
